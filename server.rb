050c098c4d7e6c914ed0861d9021d7c486192f4c
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2015-09-23 12:00:38 +0200

    station retrieval

--- server.rb
+++ server.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 set :port => 2342
 
-get '/hi' do
-  "Hello world"
+get '/connections' do
+  # matches "GET /connections?x=locx&y=locy&dest=station"
+  x = params['x'];
+  y = params['y'];
+  dest = params['dest'];
+  uri = URI("http://transport.opendata.ch/v1/locations?x=#{x}&y=#{y}")
+  response = JSON.parse(Net::HTTP.get(uri))
+  station_names = []
+  for i in 0..response['stations'].length-1
+    station_names.push(response['stations'][i]['name'])
+  end
+  response['stations'][0]['name']
 end
